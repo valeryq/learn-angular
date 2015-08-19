e362ed70878ceaf65f5199899ddc05219ae173a7
Valery Zakharchenko <v.zakharchenko@smcs.com.ua>
2015-08-19 11:08:42 +0300

    enable html5 history api

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -6,9 +6,15 @@
         .config(config);
 
     /** @ngInject */
-    function config($logProvider, webSqlProvider, DB_CONFIG) {
+    function config($logProvider, $locationProvider, webSqlProvider, DB_CONFIG) {
         // Enable log
         $logProvider.debugEnabled(true);
+
+        /* Включаем для роутов HTML5 History API */
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
 
         /* Конфигурируем webSql провайдер */
         webSqlProvider.setConfig(DB_CONFIG);
