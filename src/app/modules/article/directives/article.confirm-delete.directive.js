36a7030b68fe89e7fedcc4c621200dc7b157879a
Valery Zakharchenko <v.zakharchenko@smcs.com.ua>
2015-08-20 18:14:49 +0300

    fix formatting

--- src/app/modules/article/directives/article.confirm-delete.directive.js
+++ src/app/modules/article/directives/article.confirm-delete.directive.js
@@ -1,44 +1,42 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('blog.article')
-    .directive('articleConfirmDelete', ArticleConfirmDeleteDirective);
+    angular
+        .module('blog.article')
+        .directive('articleConfirmDelete', ArticleConfirmDeleteDirective);
 
 
-  /**
-   * Директива для confirm-диалога удаления поста
-   *
-   * @constructor
-   * @ngInject
-   */
-  function ArticleConfirmDeleteDirective($mdDialog) {
-    return {
-      priority: 1,
-      scope: {
-        callback: '&articleConfirmDelete'
-      },
-      link: ArticleConfirmDeleteDirectiveLink
-    };
+    /**
+     * Директива для confirm-диалога удаления поста
+     *
+     * @constructor
+     * @ngInject
+     */
+    function ArticleConfirmDeleteDirective($mdDialog) {
+        return {
+            priority: 1,
+            scope: {
+                callback: '&articleConfirmDelete'
+            },
+            link: ArticleConfirmDeleteDirectiveLink
+        };
 
-    function ArticleConfirmDeleteDirectiveLink(scope, element, attr) {
+        function ArticleConfirmDeleteDirectiveLink(scope, element, attr) {
 
-      var clickAction = attr.articleConfirmDelete;
+            /* Регистрируем ивент для клика по директиве */
+            element.bind('click', function (event) {
+                var confirm = $mdDialog.confirm()
+                    .title('Удаление')
+                    .content('Вы действительно хотите удалить пост?')
+                    .ok('Удалить')
+                    .cancel('Отменить')
+                    .targetEvent(event);
 
-      /* Регистрируем ивент для клика по директиве */
-      element.bind('click', function (event) {
-        var confirm = $mdDialog.confirm()
-          .title('Удаление')
-          .content('Вы действительно хотите удалить пост?')
-          .ok('Удалить')
-          .cancel('Отменить')
-          .targetEvent(event);
-
-        $mdDialog.show(confirm).then(function () {
-          scope.callback();
-        });
-      });
+                $mdDialog.show(confirm).then(function () {
+                    scope.callback();
+                });
+            });
+        }
     }
-  }
 
 })();
