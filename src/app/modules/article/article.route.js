437995a90c3eca02cf53348e7b1505fde7e6e7cd
Serj Korchma <serj.korchma@gmail.com>
2015-08-20 12:27:31 +0300

    fix routing

--- src/app/modules/article/article.route.js
+++ src/app/modules/article/article.route.js
@@ -1,71 +1,72 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('blog.article')
-        .config(routeConfig);
+  angular
+    .module('blog.article')
+    .config(routeConfig);
 
-    /** @ngInject */
-    function routeConfig($stateProvider) {
-        $stateProvider
-            .state('article', {
-                abstract: true,
-                url: '/article'
-            })
-            .state('article.list', {
-                url: '/list',
-                templateUrl: 'app/modules/article/list/list.html',
-                controller: 'ListController',
-                controllerAs: 'vm',
-                resolve: {
-                    articles: articlesList
-                }
-            })
-            .state('article.create', {
-                url: '/create',
-                templateUrl: 'app/modules/article/create/create.html',
-                controller: 'CreateController',
-                controllerAs: 'vm',
-                resolve: {
-                    article: function () {
-                        return {};
-                    }
-                }
-            })
-            .state('article.edit', {
-                url: '/edit/:id',
-                templateUrl: 'app/modules/article/create/create.html',
-                controller: 'CreateController',
-                controllerAs: 'vm',
-                resolve: {
-                    article: findArticle
-                }
-            });
-    }
+  /** @ngInject */
+  function routeConfig($stateProvider) {
+    $stateProvider
+      .state('article', {
+        abstract: true,
+        template: '<ui-view />',
+        url: '/article'
+      })
+      .state('article.list', {
+        url: '/list',
+        templateUrl: 'app/modules/article/list/list.html',
+        controller: 'ListController',
+        controllerAs: 'vm',
+        resolve: {
+          articles: articlesList
+        }
+      })
+      .state('article.create', {
+        url: '/create',
+        templateUrl: 'app/modules/article/create/create.html',
+        controller: 'CreateController',
+        controllerAs: 'vm',
+        resolve: {
+          article: function () {
+            return {};
+          }
+        }
+      })
+      .state('article.edit', {
+        url: '/edit/:id',
+        templateUrl: 'app/modules/article/create/create.html',
+        controller: 'CreateController',
+        controllerAs: 'vm',
+        resolve: {
+          article: findArticle
+        }
+      });
+  }
 
-    /**
-     * Получить промис списка постов
-     *
-     * @param ArticleService
-     * @returns {*}
-     *
-     * @ngInject
-     */
-    function articlesList(ArticleService) {
-        return ArticleService.getList();
-    }
+  /**
+   * Получить промис списка постов
+   *
+   * @param ArticleService
+   * @returns {*}
+   *
+   * @ngInject
+   */
+  function articlesList(ArticleService) {
+    return ArticleService.getList();
+  }
 
-    /**
-     * Получить промис поиска одного поста
-     *
-     * @param $route
-     * @param ArticleService
-     * @returns {*}
-     *
-     * @ngInject
-     */
-    function findArticle($route, ArticleService) {
-        return ArticleService.find($route.current.params.id);
-    }
+  /**
+   * Получить промис поиска одного поста
+   *
+   * @param $route
+   * @param ArticleService
+   * @returns {*}
+   *
+   * @ngInject
+   */
+  function findArticle($stateParams, ArticleService) {
+    return ArticleService.find($stateParams.id);
+  }
 
 })();
